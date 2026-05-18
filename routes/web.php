566d7e0b2d4cbd6e8ff1e8a32a342dea23122cdb
taxdevid <arigunatakwa669@gmail.com>
2026-05-18 11:59:06 +0700

    feat: routing auth, users, and pengaduan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\PengaduanController;
+
+Route::prefix('api')->group(function () {
+    // Auth Routes
+    Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::post('/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+    Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
+    Route::get('/me', [\App\Http\Controllers\Api\AuthController::class, 'me']);
+
+    // Pengaduan Routes
+    Route::get('/pengaduans', [PengaduanController::class, 'index']);
+    Route::post('/pengaduans', [PengaduanController::class, 'store']);
+    Route::get('/pengaduans/{nomorTiket}', [PengaduanController::class, 'show']);
+    Route::put('/pengaduans/{nomorTiket}', [PengaduanController::class, 'update']);
+    Route::delete('/pengaduans/{nomorTiket}', [PengaduanController::class, 'destroy']);
+    
+    // Users Routes
+    Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+    Route::post('/users', [\App\Http\Controllers\Api\UserController::class, 'store']);
+    Route::put('/users/{id}', [\App\Http\Controllers\Api\UserController::class, 'update']);
+    Route::patch('/users/{id}/status', [\App\Http\Controllers\Api\UserController::class, 'updateStatus']);
+    Route::delete('/users/{id}', [\App\Http\Controllers\Api\UserController::class, 'destroy']);
+});
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
